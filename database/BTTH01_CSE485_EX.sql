0a4fca6ef3182cc42d4d7e76bd135bf23d6deea5
Hoàng Minh Quân <zedloikiem666@gmail.com>
2023-02-25 19:47:58 +0700

    update

--- database/BTTH01_CSE485_EX.sql
+++ database/BTTH01_CSE485_EX.sql
@@ -25,7 +25,30 @@ SELECT * FROM baiviet WHERE ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%'
 /*i*/
 CREATE VIEW vw_Music AS SELECT bv.ma_bviet, bv.tieude,bv.ten_bhat,bv.ma_tloai,bv.tomtat,bv.noidung, bv.ma_tgia, bv.ngayviet, bv.hinhanh, tl.ten_tloai, tg.ten_tgia FROM baiviet as bv JOIN theloai as tl ON bv.ma_tloai = tl.ma_tloai JOIN tacgia as tg ON bv.ma_tgia = tg.ma_tgia;
 
+/*CREATE PROCEDURE sp_DSBaiViet
+    @ten_tloai VARCHAR(50)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM theloai WHERE ten_tloai = @ten_tloai)
+    BEGIN
+        PRINT 'Thể loại ' + @ten_tloai + ' không tồn tại.'
+        RETURN
+    END
 
+    SELECT bv.ma_bviet, bv.tieude, bv.ten_bhat, tg.ten_tgia, tl.ten_tloai, bv.ngayviet
+    FROM baiviet bv
+    INNER JOIN tacgia tg ON bv.ma_tgia = tg.ma_tgia
+    INNER JOIN theloai tl ON bv.ma_tloai = tl.ma_tloai
+    WHERE tl.ten_tloai = @ten_tloai
+END*/
+/*CREATE TRIGGER tg_CapNhatTheLoai 
+AFTER INSERT, UPDATE, DELETE ON baiviet 
+FOR EACH ROW 
+BEGIN 
+    UPDATE theloai 
+    SET soluong_bviet = (SELECT COUNT(*) FROM baiviet WHERE ma_tloai = theloai.ma_tloai)
+    WHERE ma_tloai = NEW.ma_tloai; 
+END;*/
 
 /*l*/
 CREATE TABLE Users (
