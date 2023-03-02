2f04e78e7ab6439d19456f95568b5e0cf5b400dd
long0167472 <Hoanglong7374@gmail.com>
2023-03-03 05:51:06 +0700

    Add process delete and fix something

--- admin/add_author.php
+++ admin/add_author.php
@@ -49,12 +49,12 @@
                 <form action="process_add_author.php" method="post" enctype="multipart/form-data">
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatName">Tên tác giả</span>
-                        <input type="text" class="form-control" name="txtCatName ten_tgia" id="ten_tgia" >
+                        <input type="text" class="form-control" name="ten_tgia" id="ten_tgia" >
                     </div>
 
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatName">Ảnh tác giả</span>
-                        <input type="file" class="form-control" name="txtCatName anh_tgia" >
+                        <input type="file" class="form-control" name="anh_tgia" id="anh_tgia">
                     </div>
 
                     <div class="form-group  float-end ">
